8eb6bab4a7cb965fab061df0d5b661d440eb7f68
GitHubibi <mq.smile@gmail.com>
2023-10-22 12:13:31 +0700

    hidden number

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import json
 
 def job():
     now  = datetime.datetime.now()
-    pywhatkit.sendwhatmsg("+6285326207574", "I'am phyton", 12, 7)
+    pywhatkit.sendwhatmsg("+", "I'am phyton", 12, 7)
 
 job()
 schedule.every(3).minutes.do(job)
